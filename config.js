cad22f7e316b58d0bea91b7e21168faa9949191e
kingznverlose <ibrohimmoh7@gmail.com>
2026-01-20 13:09:50 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-//credit By Limzzzhama 
-//no hapus credit ketauan hapus hitam//
-
-module.exports = {
-  domain: "https://bugvipkingz.biz.id", // Ubah jadi Domain panel Mu !!!
-  port: "19027" // Ubah Jadi Port Panel Mu !!!
-};
